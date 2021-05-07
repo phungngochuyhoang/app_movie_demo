19090e6566e4c62d2d403e1f88e2b43d34ec725a
hoang phung <hoang.phung@cloudconnecttech.com>
2021-05-07 15:36:11 +0700

    lkdgjlksdg

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 //
 import '../styles/globals.css'
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/font-awesome/css/font-awesome.min.css'
 
 function MyApp({ Component, pageProps }) {
   return (
