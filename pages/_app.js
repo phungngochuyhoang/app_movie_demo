04a29376b6771bdb0c0db3bf0498a6e9799dbe9b
hoang phung <hoang.phung@cloudconnecttech.com>
2021-05-07 15:00:29 +0700

    ccc

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,6 @@
 //
 import '../styles/globals.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/font-awesome/css/font-awesome.min.css'
 
-//
-// import Layout from './components/layout'
 
 function MyApp({ Component, pageProps }) {
   return (
