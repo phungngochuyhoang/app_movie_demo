b49dc086eebfe0e87517bb1c7167cb52b5025164
hoang phung <hoang.phung@cloudconnecttech.com>
2021-05-07 10:39:59 +0700

    add folder movie

---
+++ pages/movie/index.js
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+
+const Movie = function () {
+
+    return (<div>
+        <h1> Movie 625f9ea78e3019d21ad4ef853e109282 </h1>
+        
+    </div>)
+}
+
+export default Movie;
