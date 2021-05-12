2e781e97ce5c3f149ab9fbefc478d0bdb0b56489
hoang phung <hoang.phung@cloudconnecttech.com>
2021-05-12 15:44:56 +0700

    aa

--- pages/index.js
+++ pages/index.js
@@ -17,14 +17,15 @@ let imageSize = {
 
 
 export default function Home({ movies }) {
-
+  
   const [showHide, setShowHide] = useState(false);
   const onMenu = useCallback(() => { setShowHide(!showHide) }, [showHide])
+
 
   var listMovies = movies.results.map((movie) =>
     <div key={movie.id} className={HomeStyles.movie__box}>
       <Image
-        src={movie.backdrop_path != null ? `${process.env.URL_BASE_IMAGE}${movie.backdrop_path}` : '/image/error.jpg'}
+        src={movie.backdrop_path != null ? `${'https://image.tmdb.org/t/p/w500/'}${movie.backdrop_path}` : '/image/error.jpg'}
         title={movie.name}
         width={imageSize.width}
         height={imageSize.height} />
@@ -48,6 +49,7 @@ export default function Home({ movies }) {
 }
 
 export const getServerSideProps = async function (context) {
+  
   var res = await fetch("https://api.themoviedb.org/3/discover/tv?api_key=625f9ea78e3019d21ad4ef853e109282&all");
   var movies = await res.json();
 
